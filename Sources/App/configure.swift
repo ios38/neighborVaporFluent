92e3d7158cbd6e4eaf90e7d6ee367d4f13e6a1c0
Maksim Romanov <ios38ru@gmail.com>
2020-11-27 08:55:15 +0800

    My First Route

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,18 @@
 import Vapor
 
-// configures your application
+/*
+public func configure(
+  _ config: inout Config,
+  _ env: inout Environment,
+  _ services: inout Services
+) throws {
+  // Register routes to the router
+  let router = EngineRouter.default()
+  try routes(router)
+  services.register(router, as: Router.self)
+}*/
+
+
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,15 @@
 import Vapor
 
+/*
+public func routes(_ router: Router) throws {
+  // nothing left here
+}*/
+
+struct Person: Content {
+  var name: String
+  var age: Int
+}
+
 func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
@@ -8,4 +18,12 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("name") { req in
+      return "Max"
+    }
+    app.get("json") { req in
+      return Person(name: "Max", age: 47)
+    }
+
 }
